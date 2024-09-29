15c9da8d4da8aa46e79d32b361838f487b3e6b6f
Tayyabah <tayyabah659@gmail.com>
2024-09-29 16:44:31 +0500

    adding if and else conditions

--- script.js
+++ script.js
@@ -7,15 +7,14 @@ function calculateAge() {
     let months = today.getMonth() - birthdate.getMonth();
     let days = today.getDate() - birthdate.getDate();
 
-    // Adjust if birth date is in the future or for month/day differences
+   
     if (months < 0 || (months === 0 && days < 0)) {
       years--;
       months += 12; 
     }
     if (days < 0) {
       months--;
-      // Logic to calculate days remaining in previous month
-    }
+      }
 
     document.getElementById("years").textContent = years;
     document.getElementById("months").textContent = months;
